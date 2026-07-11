5599c5d7afe108b45a39db5d16af846ead1deb93
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-07-11 20:50:07 +0530

    bin to dec vice versa

--- Chapter 8 Binary Number/code.cpp
+++ Chapter 8 Binary Number/code.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
 using namespace std;
 
-void binToDec(int binNum)
+void decToBin(int decNum)
 {
+    int n = decNum;
 
-    int n = binNum;
-    int decNum = 0;
+    int binNum = 0;
     int pow = 1;
 
     while (n > 0)
     {
 
-        int lastDig = n % 10;
-        decNum += lastDig * pow;
-        pow *= 2;
-        n /= 10;
-    }
-    cout << decNum << endl;
-}
-
-void decToBin(int decNum)
-{
-
-    int n = decNum;
-    int pow = 1; // 1^0 , 1^2
-    int binNum = 0;
-
-    while (n > 0)
-    {
         int rem = n % 2;
         binNum += rem * pow;
         n /= 2;
         pow *= 10;
     }
-    cout << binNum;
+    cout << "Decimal to Binary : " << decNum << " "  << binNum << endl;
+}
+
+void binToDec(int binNum)
+{
+
+    int n = binNum;
+    int pow = 1;
+    int decNum = 0;
+
+    while (n > 0)
+    {
+        int lastDigit = n % 10;
+        decNum += lastDigit * pow;
+        pow *= 2;
+        n /= 10;
+    }
+    cout << "Binary to Decimal : " << binNum  << " " << decNum << endl;
 }
 
 int main()
 {
 
-    decToBin(7);
+    decToBin(5);
+    binToDec(101);
+
     return 0;
 }
