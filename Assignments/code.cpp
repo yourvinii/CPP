8d160acc2941acc5cb8bb76027b493dbfc47774a
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-07-12 17:18:11 +0530

    var + dt

---
+++ Assignments/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  
+    float r = 5.6;
+    float area = M_PI * r * r;
+    cout << area << endl;
+    cout << M_PI;
+
+
+
+
+    return 0;
+}
