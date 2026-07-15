bb58ed1566625db119be1e1180bcbf086fb2e75f
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-07-15 15:52:56 +0530

    passing arrays to any function as an argument , is pass by reference

--- Chapter 10 Arrays/code.cpp
+++ Chapter 10 Arrays/code.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void printArr(int nums[], int n)
+
+{
+
+    cout << sizeof(nums) << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << ",";
+    }
+    cout << endl;
+}
+
 int main()
 {
 
-    int arr[5] = {1, 2, 3, 4, 5};
-
+    int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(int);
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " at index : " << i  << endl;
-    }
+    // cout << sizeof(arr) << endl;
+
+    printArr(arr, n);
+
     return 0;
 }
