9a7cd1058809baa61f8ffb03a17c7e6059174564
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-07-17 11:31:12 +0530

    array pointer acts like a constant pointer

--- Chapter 10 Arrays/code.cpp
+++ Chapter 10 Arrays/code.cpp
@@ -15,7 +15,6 @@ int binSearch(int *arr, int n, int key)
 {
     int start = 0;
     int end = n - 1;
-    
 
     while (start <= end)
     {
@@ -40,11 +39,10 @@ int binSearch(int *arr, int n, int key)
 int main()
 {
 
-    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
-    int n = sizeof(arr) / sizeof(int);
-
-    cout << binSearch(arr, n, 16);
-
+    int arr[] = {5};
+    int b = 25;
+    arr = &b;
+    cout << arr;
     return 0;
 }
 
