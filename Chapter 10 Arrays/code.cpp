d585da30cce32e22bd808620796efafd9f097cb5
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-07-17 10:31:09 +0530

    reverse array two pointers approach

--- Chapter 10 Arrays/code.cpp
+++ Chapter 10 Arrays/code.cpp
@@ -6,28 +6,44 @@ void printArr(int *ptr, int n)
 
     for (int i = 0; i < n; i++)
     {
-        cout << ptr[i] << ",";
+        cout << ptr[i] << " ";
     }
+    cout << endl;
 }
 
 int main()
 {
 
-    int arr[] = {5, 4, 3, 9, 2};
+    int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(int);
 
-    int copyArr[n];
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
 
     for (int i = 0; i < n; i++)
     {
-        int j = n - i - 1;
-        copyArr[i] = arr[j];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = copyArr[i];
+        cout << arr[i] << " ";
     }
 
-    printArr(arr, n);
     return 0;
 }
+
+/*
+
+time-complexity - input size + number of operation
+
+space complexity - input size  + extra space
+
+
+
+
+*/
