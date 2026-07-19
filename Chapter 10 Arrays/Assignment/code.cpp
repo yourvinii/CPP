1b45cb765d8fae5e96e34efbbff16f800ba3a689
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-07-19 20:43:11 +0530

    subarray

--- Chapter 10 Arrays/Assignment/code.cpp
+++ Chapter 10 Arrays/Assignment/code.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
-
 using namespace std;
+
+void printSubArr(int *ptr, int n)
+{
+    for (int start = 0; start < n; start++)
+    {
+        cout << start << " = " ;
+        for (int end = start; end < n; end++)
+        {
+            // cout << "["  << end << ']';
+            for(int i=start;i<=end;i++){
+                cout << ptr[i]  ;
+            }
+            cout << ", ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
 
-
-    
-
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+    printSubArr(arr, n);
     return 0;
 }
