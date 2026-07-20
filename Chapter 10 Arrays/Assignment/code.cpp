7f3ceb306f0d5dbd2533dc374e62b7202d945b3a
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-07-20 20:33:02 +0530

    Kadane's algorithm for max sum subarray

--- Chapter 10 Arrays/Assignment/code.cpp
+++ Chapter 10 Arrays/Assignment/code.cpp
@@ -1,3 +1,6 @@
+/*
+// sub array
+
 #include <iostream>
 using namespace std;
 
@@ -21,8 +24,59 @@ void printSubArr(int *ptr, int n)
 int main()
 {
 
-    int arr[] = {1, 2, 3, 4, 5};
+int arr[] = {1, 2, 3, 4, 5};
+int n = sizeof(arr) / sizeof(int);
+printSubArr(arr, n);
+return 0;
+}
+*/
+
+//  max subarray sub
+#include <iostream>
+using namespace std;
+
+void maxSubArrSum(int *ptr, int n)
+{
+    int maxSum = INT_MIN;
+    for (int start = 0; start < n; start++)
+    {
+        for (int end = start; end < n; end++)
+        {
+            int curSum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                curSum += ptr[i];
+            }
+            cout << curSum << ", ";
+            maxSum = max(curSum, maxSum);
+        }
+        cout << endl;
+    }
+    cout << "Maximum Sub array sum : " << maxSum << endl;
+}
+
+void kadAlog(int *ptr, int n)
+{
+    int currSum = 0;
+    int maxSum = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        currSum += ptr[i];
+        if (currSum < 0)
+        {
+            currSum = 0;
+        }
+        maxSum = max(currSum ,maxSum);
+    }
+    cout << "maximum subarray sum : " << maxSum ;
+}
+
+int main()
+{
+
+    int arr[] = {2, -3, 6, -5, 4, 2};
     int n = sizeof(arr) / sizeof(int);
-    printSubArr(arr, n);
+
+    kadAlog(arr, n);
     return 0;
 }
